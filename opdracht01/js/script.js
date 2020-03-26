7ac036c29ffc1ffd17cb1373cbffa6c72a5d3524
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-03-26 20:30:09 +0100

    tot en met opdracht 3 gemaakt

--- opdracht01/js/script.js
+++ opdracht01/js/script.js
@@ -2,9 +2,9 @@ var stop = "rood";
 var maybe = "oranje";
 var go = "groen";
 
-window.onload = function(){
-    //JOUW CODE HIER
-
-    
-}
-
+window.onload = function() {
+  //JOUW CODE HIER
+  document.getElementById("light-top").innerHTML = stop;
+  document.getElementById("light-middle").innerHTML = maybe;
+  document.getElementById("light-bottom").innerHTML = go;
+};
--- opdracht02/js/script.js
+++ opdracht02/js/script.js
@@ -2,11 +2,13 @@ var stop = "rood";
 var maybe = "oranje";
 var go = "groen";
 
-window.onload = function(){
-    document.getElementById("light-top").innerHTML = stop;
-    document.getElementById("light-middle").innerHTML = maybe;
-    document.getElementById("light-bottom").innerHTML = go;
+window.onload = function() {
+  document.getElementById("light-top").innerHTML = stop;
+  document.getElementById("light-middle").innerHTML = maybe;
+  document.getElementById("light-bottom").innerHTML = go;
 
-    //JOUW CODE HIER
-}
-
+  //JOUW CODE HIER
+  document.getElementById("light-top").style.backgroundColor = "red";
+  document.getElementById("light-middle").style.backgroundColor = "orange";
+  document.getElementById("light-bottom").style.backgroundColor = "green";
+};
