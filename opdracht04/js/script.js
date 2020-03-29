5453a7fa5a97bd98d484fac44cbad3cd16aa3643
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-03-29 01:35:45 +0100

    opdracht 4 KLAAR!

--- opdracht04/js/script.js
+++ opdracht04/js/script.js
@@ -2,25 +2,32 @@ var stop = "rood";
 var maybe = "oranje";
 var go = "groen";
 
-window.onload = function(){
-    document.getElementById("light-top").innerHTML = stop;
-    document.getElementById("light-middle").innerHTML = maybe;
-    document.getElementById("light-bottom").innerHTML = go;
+window.onload = function() {
+  document.getElementById("light-top").innerHTML = stop;
+  document.getElementById("light-middle").innerHTML = maybe;
+  document.getElementById("light-bottom").innerHTML = go;
+};
+
+function zetRoodLichtAan() {
+  if (document.getElementById("light-top").style.backgroundColor == "gray") {
+    document.getElementById("light-top").style.backgroundColor = "red";
+  } else {
+    document.getElementById("light-top").style.backgroundColor = "gray";
+  }
 }
 
-function zetRoodLichtAan(){
-    if(document.getElementById("light-top").style.backgroundColor == "gray"){
-        document.getElementById("light-top").style.backgroundColor = "red";
-    }
-    else{
-        document.getElementById("light-top").style.backgroundColor = "gray";
-    }
-}
-
-function zetOranjeLichtAan(){
+function zetOranjeLichtAan() {
+  if (document.getElementById("light-middle").style.backgroundColor == "gray") {
     document.getElementById("light-middle").style.backgroundColor = "orange";
+  } else {
+    document.getElementById("light-middle").style.backgroundColor = "gray";
+  }
 }
 
-function zetGroenLichtAan(){
+function zetGroenLichtAan() {
+  if (document.getElementById("light-bottom").style.backgroundColor == "gray") {
     document.getElementById("light-bottom").style.backgroundColor = "green";
+  } else {
+    document.getElementById("light-bottom").style.backgroundColor = "gray";
+  }
 }
